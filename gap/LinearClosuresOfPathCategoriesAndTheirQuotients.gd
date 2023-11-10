19cfcfe00f761b9b28fa64b80a22973c3c6d4b2c
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2023-11-10 15:41:52 +0100

    extracted new package FpCategories from Algebroids

---
+++ gap/LinearClosuresOfPathCategoriesAndTheirQuotients.gd
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# FpCategories: Finitely presented categories by generating quivers and relations
+#
+# Declarations
+#
+
+DeclareGlobalFunction( "INSTALL_VIEW_AND_DISPLAY_METHODS_IN_LINEAR_CLOSURES_OF_PATH_CATEGORIES_OR_THEIR_QUOTIENTS" );
+
