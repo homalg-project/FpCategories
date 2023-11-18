c02760120ac81af526d7485b48bf6470e4836eb6
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2023-11-18 15:57:37 +0100

    moved LinearClosuresOfPathCategoriesAndTheirQuotients* back to Algebroids

--- gap/LinearClosuresOfPathCategoriesAndTheirQuotients.gd
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: GPL-2.0-or-later
-# FpCategories: Finitely presented categories by generating quivers and relations
-#
-# Declarations
-#
-
-DeclareGlobalFunction( "INSTALL_VIEW_AND_DISPLAY_METHODS_IN_LINEAR_CLOSURES_OF_PATH_CATEGORIES_OR_THEIR_QUOTIENTS" );
-
